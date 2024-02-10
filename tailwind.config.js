f3c5177501b489137e5bb82594d97eb9c3757e35
schinak-m <shinak.x3@gmail.com>
2024-02-11 01:37:39 +0300

    Completed Assignment

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Open Sans'],
+        headings: ['Poppins']
+      }
+    },
+  },
+  plugins: [],
+}
